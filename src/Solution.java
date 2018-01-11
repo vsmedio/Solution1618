1207f8cd1aad31367833dfe3fbab9df6835b5b95
Arvis Abromovics <info@vsmedio.lv>
2018-01-11 16:00:53 +0100

    Initial commit

---
+++ src/Solution.java
@@ -0,0 +1,46 @@
+public class Solutiona/*
+Снова interrupt
+Создай нить TestThread.
+В методе main создай экземпляр нити, запусти, а потом прерви ее используя метод interrupt().
+
+
+Требования:
+1. Класс TestThread должен быть унаследован от Thread.
+2. Класс TestThread должен иметь public void метод run.
+3. Метод main должен создавать объект типа TestThread.
+4. Метод main должен вызывать метод start у объекта типа TestThread.
+5. Метод main должен вызывать метод interrupt у объекта типа TestThread.
+
+
+public class Solution {
+    public static void main(String[] args) throws InterruptedException {
+        //Add your code here - добавь код тут
+    }
+
+    //Add your code below - добавь код ниже
+    public static class TestThread {
+    }
+}
+
+*/
+
+public class Solution {
+    public static void main(String[] args) throws InterruptedException {
+        TestThread thread = new TestThread();
+        thread.start();
+        Thread.sleep(1);
+        thread.interrupt();
+    }
+
+    public static class TestThread extends Thread {
+        @Override
+        public void run() {
+            while (true) {
+                System.out.print(".");
+                if (isInterrupted())
+                    break;
+            }
+        }
+    }
+} {
+}
